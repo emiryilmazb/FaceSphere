34615c708c520baa3e9697ed3c088d10425ffd5b
Yilmaz Emir (BSH GDS-PSUE) <yei4ib@bosch.com>
2024-03-06 11:09:03 +0300

    initial commit

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 face_recognizer.py
-
 import subprocess
-
 while True:
     user_input = input("Press 't' to trigger face recognition process: ")
     if user_input.lower() == 't':
@@ -9,18 +7,14 @@ while True:
 
 face_taker.py
 import subprocess
-
 # Burada yüz tespiti işlemleri yapılır.
-
 # Ardından eğitim işlemi başlatılır.
 subprocess.run(["python", "face_train.py"])
-
 # Eğitim tamamlandıktan sonra face_recognizer.py betiğini tekrar çağırır.
 subprocess.run(["python", "face_recognizer.py"])
 
 face_train.py
 # Yüz eğitimi işlemleri yapılır.
-
 # Eğitim tamamlandıktan sonra face_recognizer.py betiğini tekrar çağırır.
 import subprocess
 subprocess.run(["python", "face_recognizer.py"])
